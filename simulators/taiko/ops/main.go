6c98aa2b6e75d4711771fe06d13d04035d1650ee
phenix3443 <phenix3443@gmail.com>
2022-12-19 10:43:19 +0800

    feat: add sync tests

--- simulators/taiko/ops/main.go
+++ simulators/taiko/ops/main.go
@@ -10,7 +10,6 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/hive/hivesim"
 	"github.com/ethereum/hive/taiko"
@@ -61,7 +60,7 @@ func firstL2Block(t *hivesim.T) {
 	env.StartSingleNodeNet(t)
 
 	// generate the first L2 transaction
-	env.L2Vault.CreateAccount(ctx, env.Net.GetL2ELNode(0).EthClient(t), big.NewInt(params.Ether))
+	env.L2Vault.SendL2TestTx(ctx, env.Net.GetL2ELNode(0).EthClient(t))
 
 	t.Run(hivesim.TestSpec{
 		Name:        "first L1 block",
@@ -102,7 +101,7 @@ func syncL2Block(t *hivesim.T) {
 	env := taiko.NewTestEnv(ctx, t, taiko.DefaultConfig)
 	env.StartSingleNodeNet(t)
 
-	blockCnt := uint64(20)
+	blockCnt := uint64(10)
 	env.GenSomeL2Blocks(t, blockCnt)
 
 	t.Run(hivesim.TestSpec{
@@ -115,11 +114,11 @@ func syncL2Block(t *hivesim.T) {
 		Description: "L2 chain head determined by L1, but sync block completes through taiko-geth P2P",
 		Run:         syncByP2P(t, env),
 	})
-	t.Run(hivesim.TestSpec{
-		Name:        "l1Reorg",
-		Description: "L2 chain head determined by L1, but sync block completes through taiko-geth P2P",
-		Run:         l1Reorg(t, env),
-	})
+	// t.Run(hivesim.TestSpec{
+	// 	Name:        "l1Reorg",
+	// 	Description: "L2 chain head determined by L1, but sync block completes through taiko-geth P2P",
+	// 	Run:         l1Reorg(t, env),
+	// })
 }
 
 func syncAllFromL1(t *hivesim.T, env *taiko.TestEnv, height uint64) func(*hivesim.T) {
@@ -134,29 +133,66 @@ func syncAllFromL1(t *hivesim.T, env *taiko.TestEnv, height uint64) func(*hivesi
 func syncByP2P(t *hivesim.T, env *taiko.TestEnv) func(*hivesim.T) {
 	return func(t *hivesim.T) {
 		ctx, d := env.Context, env.Net
-		// start new L2 engine and driver to sync by p2p
+		// 1. start new L2 engine and driver to sync by p2p
 		newL2 := taiko.NewL2ELNode(t, env, d.GetL2ENodes(t))
-		taiko.NewDriverNode(t, env, d.GetL1ELNode(0), newL2, true)
-
+		newDriver := taiko.NewDriverNode(t, env, d.GetL1ELNode(0), newL2, true)
+		// 2. newL2 should sync to LatestVerifiedHeight by p2p
 		taikoL1 := d.GetL1ELNode(0).TaikoL1Client(t)
 		l1State, err := rpc.GetProtocolStateVariables(taikoL1, nil)
 		require.NoError(t, err)
-		taiko.WaitHeight(ctx, t, newL2, taiko.GreaterEqual(l1State.LatestVerifiedHeight))
+		heightOfP2PSyncTo := l1State.LatestVerifiedHeight
+		taiko.WaitHeight(ctx, t, newL2, taiko.GreaterEqual(heightOfP2PSyncTo))
+		// 3. newDriver should sync one by one from L1
+		blockCnt := uint64(10)
+		env.GenSomeL2Blocks(t, blockCnt)
+		heightOfOneByOne := heightOfP2PSyncTo + blockCnt
+		taiko.WaitHeight(ctx, t, newL2, taiko.GreaterEqual(heightOfOneByOne))
+		// 4. stop newDriver
+		t.Sim.StopClient(t.SuiteID, t.TestID, newDriver.Container)
+		// 5. restart newDriver, newL2 should sync to latest header by p2p
+		env.GenSomeL2Blocks(t, blockCnt)
+		taiko.NewDriverNode(t, env, d.GetL1ELNode(0), newL2, true)
+		heightOfResume := heightOfOneByOne + blockCnt
+		taiko.WaitHeight(ctx, t, newL2, taiko.GreaterEqual(heightOfResume))
 	}
 }
 
 func l1Reorg(t *hivesim.T, env *taiko.TestEnv) func(t *hivesim.T) {
 	return func(t *hivesim.T) {
-		l1 := env.Net.GetL1ELNode(0)
-		taikoL1 := l1.TaikoL1Client(t)
-		l1State, err := rpc.GetProtocolStateVariables(taikoL1, nil)
+		l1, l2, ctx := env.Net.GetL1ELNode(0), env.Net.GetL2ELNode(0), env.Context
+		l2Height, err := l2.EthClient(t).BlockNumber(ctx)
 		require.NoError(t, err)
+		resetL2Height := l2Height / 2
+		t.Logf("resetL2Height=%v", resetL2Height)
+		info, err := l2.EthClient(t).L1OriginByID(ctx, big.NewInt(int64(resetL2Height)))
+		require.NoError(t, err)
+		resetL1Height := info.L1BlockHeight
+		t.Logf("resetL1Height=%v", resetL1Height)
+		l1Cli := l1.EthClient(t)
 		l1GethCli := l1.GethClient()
-		require.NoError(t, l1GethCli.SetHead(env.Context, big.NewInt(int64(l1State.GenesisHeight))))
-		l2 := env.Net.GetL2ELNode(0)
-		taiko.WaitHeight(env.Context, t, l2, func(get uint64) bool {
-			return get == 0
-		})
+
+		height, err := l1Cli.BlockNumber(ctx)
+		require.NoError(t, err)
+		nonceBeforeReorg, err := l1Cli.NonceAt(ctx, taiko.VaultAddr, big.NewInt(int64(height)))
+		require.NoError(t, err)
+		t.Logf("before revert nonce=%v", nonceBeforeReorg)
+
+		require.NoError(t, l1GethCli.SetHead(ctx, resetL1Height))
+		for {
+			l1Current, err := l1Cli.BlockNumber(ctx)
+			require.NoError(t, err)
+			t.Logf("l1Current=%v", l1Current)
+
+			env.L1Vault.SendL1TestTx(ctx, l1Cli, nonceBeforeReorg+1)
+			L2Height, err := l2.EthClient(t).BlockNumber(ctx)
+			require.NoError(t, err)
+			if L2Height == resetL2Height {
+				break
+			}
+			t.Logf("l2Current=%v", L2Height)
+			time.Sleep(10 * time.Second)
+			continue
+		}
 	}
 }
 
@@ -175,12 +211,12 @@ func tooManyPendingBlocks(t *hivesim.T) {
 
 	taikoL1 := l1.TaikoL1Client(t)
 	for canPropose(t, env, taikoL1) {
-		require.NoError(t, env.L2Vault.SendTestTx(ctx, l2.EthClient(t)))
+		require.NoError(t, env.L2Vault.SendL2TestTx(ctx, l2.EthClient(t)))
 		require.NoError(t, prop.ProposeOp(env.Context))
 		time.Sleep(10 * time.Millisecond)
 	}
 	// wait error
-	require.NoError(t, env.L2Vault.SendTestTx(ctx, l2.EthClient(t)))
+	require.NoError(t, env.L2Vault.SendL2TestTx(ctx, l2.EthClient(t)))
 	err := prop.ProposeOp(env.Context)
 	require.Error(t, err)
 	require.True(t, strings.Contains(err.Error(), "L1:tooMany"))
