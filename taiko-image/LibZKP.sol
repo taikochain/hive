6ce6b3ae6a5aa5464c0da340f4fd826f9bf4a8ab
alexshliu <104080237+alexshliu@users.noreply.github.com>
2023-01-31 15:42:44 +0800

    feat: update l1 image (#69)
    feat: update l1 image  for taiko-mono and go-ethereum latest docker image
    
    ---------
    
    Co-authored-by: phenix3443 <phenix3443@gmail.com>

---
+++ taiko-image/LibZKP.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+//  _____     _ _         _         _
+// |_   _|_ _(_) |_____  | |   __ _| |__ ___
+//   | |/ _` | | / / _ \ | |__/ _` | '_ (_-<
+//   |_|\__,_|_|_\_\___/ |____\__,_|_.__/__/
+
+pragma solidity ^0.8.9;
+
+library LibZKP {
+    /*********************
+     * Public Functions  *
+     *********************/
+
+    function verify(
+        address plonkVerifier,
+        bytes calldata zkproof,
+        bytes32 blockHash,
+        address prover,
+        bytes32 txListHash
+    ) internal view returns (bool verified) {
+       return true;
+    }
+}
