25b9650d200c75d56827ed52c36ccc082e0828ed
alexshliu <104080237+alexshliu@users.noreply.github.com>
2023-02-01 14:41:05 +0800

    fix: checkout branch bug (#72)
    Co-authored-by: phenix3443 <phenix3443@gmail.com>

--- taiko-image/build-client-image.sh
+++ taiko-image/build-client-image.sh
@@ -17,7 +17,7 @@ fi
 build_client_image() {
     client_dir="${tmp_dir}/taiko-client"
     rm -fr "${client_dir}"
-    git clone --depth=1 https://github.com/taikoxyz/taiko-client.git "${client_dir}"
+    git clone https://github.com/taikoxyz/taiko-client.git "${client_dir}"
     cd "${client_dir}"
     git checkout "${client_branch}"
     docker build -t taiko-client:local .
