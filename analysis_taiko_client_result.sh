d80d46c08c68ba93f1b4ef7d160595b52b7f0971
phenix3443 <phenix3443@gmail.com>
2023-02-08 11:05:00 +0800

    feat: handle timeout

---
+++ analysis_taiko_client_result.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+log_dir=$1
+
+for log in "${log_dir}"/*-*.json; do
+    if [ ! -f "${log}" ]; then
+        echo "log not exists"
+        break
+    fi
+    log_name=$(basename "${log}")
+    failed=$(jq '.testCases | map_values(.summaryResult.pass) | with_entries(select(.value==false)) | length' "${log}")
+    cp -r "${log_dir}"/* workspace/logs/
+    echo "failed: ${failed}, detail: http://hive.a1.taiko.xyz/?page=v-pills-results-tab&suite=${log_name}"
+done
+
+rm -r "${log_dir}"
